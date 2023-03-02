2907603648b075c70721329e06d9bfce0bbd91d3
Jos� Miguel Miguel <>
2023-03-02 15:04:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Web Page</title>
+	<style>
+		body {
+			background-color: lightblue;
+			font-family: Arial, sans-serif;
+		}
+		
+		h1 {
+			color: navy;
+			text-align: center;
+		}
+		
+		p {
+			font-size: 18px;
+			line-height: 1.5;
+		}
+		
+		ul {
+			list-style-type: none;
+			margin: 0;
+			padding: 0;
+		}
+		
+		li {
+			display: inline-block;
+			margin: 0 10px;
+		}
+		
+		a {
+			text-decoration: none;
+			color: navy;
+		}
+		
+		.btn {
+			display: inline-block;
+			padding: 10px 20px;
+			background-color: navy;
+			color: white;
+			border-radius: 5px;
+			text-decoration: none;
+			margin-top: 20px;
+		}
+		
+		.btn:hover {
+			background-color: darkblue;
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<h1>Welcome to My Web Page!</h1>
+	</header>
+	
+	<nav>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</nav>
+	
+	<main>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacinia metus id risus congue, vel tincidunt massa imperdiet. Duis posuere nulla a felis posuere, sit amet semper risus blandit. Sed bibendum sapien a nunc malesuada, at consequat turpis elementum. Suspendisse sed mauris nec justo sagittis bibendum. Vivamus in blandit velit, non lobortis risus. Nam vel lacinia risus. Donec vitae faucibus dolor, sit amet iaculis magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam eleifend nibh non lorem sagittis,
